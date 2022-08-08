12ca95ecddee36b38f80f5675094135d8dc8fb1a
Konadu Lord Kweku <yrnlehjend@gmail.com>
2022-08-08 20:57:08 +0000

    Add static files

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import page.views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', page.views.homepage, name= "home")
-]
+] + static(settings.STATIC_ROOT,document_root=settings.STATIC_ROOT)
